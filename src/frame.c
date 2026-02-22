77aac20ce6ea305e7b48d1bb8980ab9656aebf76
Stéphane Marks <shipmints@gmail.com>
2026-02-22 10:33:34 +0100

    New user option 'after-delete-frame-select-mru-frame' (bug#80397)
    Users can revert to the old 'delete-frame' behavior of selecting
    the oldest frame in 'frame-list' rather than the most-recently
    used frame.
    
    * src/frame.c (delete_frame): Consult
    'after-delete-frame-select-mru-frame'.
    (syms_of_frame): New defvar
    'after-delete-frame-select-mru-frame'.
    * etc/NEWS: Reflect the new user option.

--- src/frame.c
+++ src/frame.c
@@ -2749,7 +2749,9 @@ delete_frame (Lisp_Object frame, Lisp_Object force)
       else
 	{
 	  frame1 = Qnil;
-	  if (!EQ (force, Qnoelisp))
+
+	  if (after_delete_frame_select_mru_frame
+	      && !EQ (force, Qnoelisp))
 	    {
 	      /* Find the most recently used visible frame among all
 		 frames on the same terminal as FRAME, excluding FRAME
@@ -7777,6 +7779,13 @@ The default is \\+`inhibit' in NS builds and nil everywhere else.  */);
 #else
   alter_fullscreen_frames = Qnil;
 #endif
+
+  DEFVAR_BOOL ("after-delete-frame-select-mru-frame",
+	       after_delete_frame_select_mru_frame,
+	       doc: /* Non-nil means `delete-frame' selects most recently used frame.
+If this is nil, `delete-frame' will select the oldest visible frame on
+the same terminal.  */);
+  after_delete_frame_select_mru_frame = true;
 
   defsubr (&Sframe_id);
   defsubr (&Sframep);
